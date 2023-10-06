449692e6754362d3b0519179a35776e213a5f063
Ogedegbe Eric <ohimaigold@gmail.com>
2023-10-06 21:12:12 +0000

    update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -3,13 +3,16 @@
 '''
 import typing
 
+
 T = typing.TypeVar('T')
 
+
 def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
-                        typing.Union[T, None] = None) -> \
+                     typing.Union[T, None] = None) -> \
         typing.Union[typing.Any, T]:
 
-        if Key in dct:
-            return dct[key]
-        else:
-            return default
+
+    if Key in dct:
+        return dct[key]
+    else:
+        return default
