8826b100e0b9315f0decd3df15647d079feb969a
Ogedegbe Eric <ohimaigold@gmail.com>
2023-10-06 21:06:40 +0000

    update

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,9 +1,15 @@
 #!/usr/bin/env python3
 '''Task 11's module.
 '''
-from typing import Any, Mapping, Union, TypeVar
+import typing
 
+T = typing.TypeVar('T')
 
-T = TypeVar('T')
-Res = Union[Any, T]
-Def = Union[T, None]
+def safely_get_value(dct: typing.Mapping, key: typing.Any, default:
+                        typing.Union[T, None] = None) -> \
+        typing.Union[typing.Any, T]:
+
+        if Key in dct:
+            return dct[key]
+        else:
+            return default
